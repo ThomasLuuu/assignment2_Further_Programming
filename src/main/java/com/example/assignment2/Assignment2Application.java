44cb86dbf7af7005caaa1141203f659b87ce8f71
Trung Tín <s3752975@rmit.edu.vn>
2022-05-04 17:21:05 +0700

    Hello World

--- src/main/java/com/example/assignment2/Assignment2Application.java
+++ src/main/java/com/example/assignment2/Assignment2Application.java
@@ -8,6 +8,7 @@ public class Assignment2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment2Application.class, args);
+		System.out.println("Hello World");
 	}
 
 }
